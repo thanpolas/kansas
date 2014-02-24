fe7d4afc1038ef8275f50d9c2ceb3c96dda0e670
Thanasis Polychronakis <thanpolas@gmail.com>
2014-02-24 22:03:46 +0200

    create period-bucket test and fix bugs found

---
+++ test/unit/period-bucket-model.test.js
@@ -0,0 +1,22 @@
+/**
+ * @fileOverview Usage model unit tests.
+ */
+
+var chai = require('chai');
+var assert = chai.assert;
+
+var period = require('../../lib/models/period-bucket.model');
+
+var fixtures = require('../lib/fixtures');
+
+suite('Period Bucket Model', function() {
+
+  test.only('period.getFuture() returns expected result', function() {
+    var res = period.getFuture('month', 1);
+    var items = res.split('-');
+    assert.lengthOf(items, 3);
+    assert.lengthOf(items[0], 4);
+    assert.lengthOf(items[1], 2);
+    assert.equal(items[2], '01');
+  });
+});
