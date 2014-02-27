accffe74d8b4c280bc375417f75a95a76bf6b518
Thanasis Polychronakis <thanpolas@gmail.com>
2014-02-27 23:01:09 +0200

    write boot API test and fix bugs found

--- test/api/kansas.test.js
+++ test/api/kansas.test.js
@@ -43,6 +43,33 @@ describe('Surface tests', function () {
     expect(api.error.Policy).to.be.a('function', 'error.Policy()');
     expect(api.error.TokenNotExists).to.be.a('function', 'error.TokenNotExists()');
     expect(api.error.UsageLimit).to.be.a('function', 'error.UsageLimit()');
-
   });
+
+  it('should have the expected methods after connection', function(done) {
+    var api = kansas();
+    api.connect().then(function() {
+      expect(api.conn).to.be.an('Object', 'conn');
+
+      // token methods
+      expect(api.create).to.be.a('function', 'create');
+      expect(api.set).to.be.a('function', 'set');
+      expect(api.get).to.be.a('function', 'get');
+      expect(api.getByOwnerId).to.be.a('function', 'getByOwnerId');
+      expect(api.del).to.be.a('function', 'del');
+      expect(api.changePolicy).to.be.a('function', 'changePolicy');
+      expect(api.consume).to.be.a('function', 'consume');
+
+      // policy methods
+      expect(api.policy.create).to.be.a('function', 'policy.create');
+      expect(api.policy.get).to.be.a('function', 'policy.get');
+      expect(api.policy.has).to.be.a('function', 'policy.del');
+      expect(api.policy.model).to.be.an('object', 'policy.model');
+
+      // DB Maintenance
+      expect(api.db.prepopulate).to.be.a('function', 'db.prepopulate');
+      expect(api.db.nuke).to.be.a('function', 'db.nuke');
+      expect(api.tokenModel).to.be.a('object', 'tokenModel');
+    }).then(done, done);
+  });
+
 });
