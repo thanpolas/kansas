675ca9ff3170c916f6d580bd7c610799e938a7b9
Thanasis Polychronakis <thanpolas@gmail.com>
2014-03-01 19:18:52 +0200

    complete logger tests

--- test/api/logger.test.js
+++ test/api/logger.test.js
@@ -5,17 +5,40 @@ var Promise = require('bluebird');
 var chai = require('chai');
 var expect = chai.expect;
 
+var kansas = require('../..');
 var fixtures = require('../lib/fixtures-api');
 
 describe.only('Logger', function() {
   this.timeout(4000);
-  var fix;
 
-  fixtures.setupCase(function(res) {
-    fix = res;
+  it('Will emit "message" events', function(done) {
+    var api = kansas({console: false});
+    api.on('message', function(msgObj) {
+      // Sample Object
+      //
+      // { 
+      //   level: 200,
+      //   name: 'kansas.main.redis',
+      //   meta: undefined,
+      //   rawArgs: 
+      //    [ 'getClient() :: Creating client using host, port:',
+      //      'localhost',
+      //      6379 ],
+      //   date: Sat Mar 01 2014 19:14:47 GMT+0200 (EET),
+      //   message: 'getClient() :: Creating client using host, port: localhost 6379'
+      // }
+      expect(msgObj).to.be.an('Object');
+      expect(msgObj).to.have.property('level');
+      expect(msgObj).to.have.property('name');
+      expect(msgObj).to.have.property('date');
+      expect(msgObj).to.have.property('message');
+    });
+    api.connect().then(done.bind(null, null), done);
   });
 
-  it('FIXME', function(done) {
-    done()
+  it('Will not emit any "message" events if logging is off', function(done) {
+    var api = kansas({logging: false});
+    api.on('message', done);
+    api.connect().then(done.bind(null, null), done);
   });
 });
