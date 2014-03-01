4c544741bb2bd683d32ad0ffac733a0c2c273408
Thanasis Polychronakis <thanpolas@gmail.com>
2014-03-01 16:25:05 +0200

    scaffold logger test

--- test/api/events.test.js
+++ test/api/events.test.js
@@ -11,7 +11,7 @@ var policyAssert = require('../lib/policy-assertions.js');
 
 function noop() {}
 
-describe.only('Events', function() {
+describe('Events', function() {
   this.timeout(4000);
   var fix;
 
---
+++ test/api/logger.test.js
@@ -0,0 +1,21 @@
+/**
+ * @fileOverview Kansas logger facilities.
+ */
+var Promise = require('bluebird');
+var chai = require('chai');
+var expect = chai.expect;
+
+var fixtures = require('../lib/fixtures-api');
+
+describe('Logger', function() {
+  this.timeout(4000);
+  var fix;
+
+  fixtures.setupCase(function(res) {
+    fix = res;
+  });
+
+  it('FIXME', function() {
+    throw new Error('implement');
+  });
+});
