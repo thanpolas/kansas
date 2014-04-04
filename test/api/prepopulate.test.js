0d7f8640b21e0027dfe7bb58a1451167579971e3
Thanasis Polychronakis <thanpolas@gmail.com>
2014-04-04 21:41:27 +0300

    add prepopulation count policy tests

--- test/api/prepopulate.test.js
+++ test/api/prepopulate.test.js
@@ -33,22 +33,29 @@ describe('Prepopulation of usage keys', function() {
     it('check prepopulation works', function(done) {
       log.fine('test() :: starting prepopulate test...');
 
+      var self = this;
       fix.api.db.prepopulate().then(function() {
         log.fine('test() :: prepopulate() done!');
         var keys = fix.api.tokenModel.getKeys(fix.tokenItem);
         var keysTwo = fix.api.tokenModel.getKeys(fix.tokenItemTwo);
         var pget = Promise.promisify(fix.client.get, fix.client);
 
+        var keysCount = self.kansas.tokenModel.getKeys(self.tokenItemCount);
+
         return Promise.all([
           pget(keys.usage),
           pget(keys.usageFuture),
           pget(keysTwo.usage),
           pget(keysTwo.usageFuture),
+          pget(keysCount.usage),
+          pget(keysCount.usageFuture),
         ]).then(function(result) {
           expect(result[0]).to.equal('10', 'usage one');
           expect(result[1]).to.equal('10', 'usage one future');
           expect(result[2]).to.equal('10', 'usage two');
           expect(result[3]).to.equal('10', 'usage two future');
+          expect(result[4]).to.equal('1', 'usage count');
+          expect(result[5]).to.equal('1', 'usage count future');
         });
         // you are here
       }).then(done, done);
