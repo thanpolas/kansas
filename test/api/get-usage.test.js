b7c6674a21234a88aeaa51316bdf824cc7ba0f6b
Thanasis Polychronakis <thanpolas@gmail.com>
2014-08-07 21:22:44 +0300

    Create get-usage test and fix bugs found

---
+++ test/api/get-usage.test.js
@@ -0,0 +1,66 @@
+
+/**
+ * @fileOverview Usage model unit tests.
+ */
+// var Promise = require('bluebird');
+var chai = require('chai');
+// var sinon = require('sinon');
+// var floordate = require('floordate');
+var expect = chai.expect;
+
+// var kansasError = require('../../lib/util/error');
+var fixtures = require('../lib/fixtures-api');
+
+describe('Get Usage Model', function() {
+  this.timeout(4000);
+
+  fixtures.setupCase();
+
+  describe.only('Getting Usage', function () {
+    beforeEach(function (done) {
+      this.kansas.consume(this.token, 3)
+        .then(done.bind(null, null), done);
+    });
+    beforeEach(function (done) {
+      this.kansas.count(this.tokenItemCount.token, 4)
+        .then(done.bind(null, null), done);
+    });
+
+    it('Gets usage for a [limit] token', function(done) {
+      this.kansas.getUsage(this.token).then(function(remaining) {
+        expect(remaining).to.equal('7');
+      }).then(done, done);
+    });
+    it('Gets usage for a [limit] user id', function(done) {
+      this.kansas.getUsageByOwnerId(this.tokenItem.ownerId)
+        .bind(this)
+        .then(function(remaining) {
+          expect(remaining).to.have.length(2);
+          remaining.forEach(function(tokenItem) {
+            expect(tokenItem.usage).to.be.a('string');
+            if (tokenItem.token === this.token) {
+              expect(tokenItem.usage).to.equal('7');
+            }
+          }, this);
+        }).then(done, done);
+    });
+    it('Gets usage for a [count] token', function(done) {
+      this.kansas.getUsage(this.tokenItemCount.token).then(function(used) {
+        expect(used).to.equal('5');
+      }).then(done, done);
+    });
+    it('Gets usage for a [count] user id', function(done) {
+      this.kansas.getUsageByOwnerId(this.tokenItemCount.ownerId)
+        .bind(this)
+        .then(function(results) {
+          expect(results).to.have.length(1);
+          results.forEach(function(tokenItem) {
+            expect(tokenItem.usage).to.be.a('string');
+            if (tokenItem.token === this.tokenItemCount.token) {
+              expect(tokenItem.usage).to.equal('5');
+            }
+          }, this);
+        }).then(done, done);
+    });
+  });
+});
--- test/api/usage.test.js
+++ test/api/usage.test.js
@@ -102,37 +102,5 @@ describe('Usage Model', function() {
         }).then(done, done);
     });
   });
-
-  describe.only('Getting Usage', function () {
-    beforeEach(function (done) {
-      this.kansas.consume(this.token, 3)
-        .then(done.bind(null, null), done);
-    });
-    beforeEach(function (done) {
-      this.kansas.count(this.tokenItemCount.token, 4)
-        .then(done.bind(null, null), done);
-    });
-
-    it('Gets usage for a [limit] token', function(done) {
-      this.kansas.getUsage(this.token).then(function(remaining) {
-        expect(remaining).to.equal(7);
-      }).then(done, done);
-    });
-    it('Gets usage for a [limit] user id', function(done) {
-      this.kansas.getUsageByOwnerId(this.tokenItem.ownerId).then(function(remaining) {
-        expect(remaining).to.equal(7);
-      }).then(done, done);
-    });
-    it('Gets usage for a [count] token', function(done) {
-      this.kansas.getUsage(this.tokenItemCount.token).then(function(used) {
-        expect(used).to.equal(4);
-      }).then(done, done);
-    });
-    it('Gets usage for a [count] user id', function(done) {
-      this.kansas.getUsageByOwnerId(this.tokenItemCount.ownerId).then(function(used) {
-        expect(used).to.equal(4);
-      }).then(done, done);
-    });
-  });
 });
 
