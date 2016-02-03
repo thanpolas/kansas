d88bd8e724cc8bfde97245257889cd6457c9a6fd
Thanasis Polychronakis <thanpolas@gmail.com>
2016-02-03 23:17:51 +0200

    set timeout in grunt mocha

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,7 @@ module.exports = function (grunt) {
       options: {
         ui: 'bdd',
         clearRequireCache: true,
+        timeout: 10000,
       },
       api: {
         src: ['test/api/*.js']
